995db49acbe12f71892c00d0e793baac7fa21128
Thomas Faria <thomas.faria@ensae.fr>
2022-11-11 15:48:03 +0100

    using data.table instead to speed up computations

--- R/compute_growth_rate.R
+++ R/compute_growth_rate.R
@@ -3,45 +3,36 @@ library(dplyr)
 library(xts)
 library(tidyr)
 library(stringr)
+library(data.table)
 
-data <- arrow::read_csv_arrow("https://minio.lab.sspcloud.fr/tfaria/public/RealTimeDatabase.csv")
+data <- arrow::read_csv_arrow("https://minio.lab.sspcloud.fr/tfaria/public/RealTimeDatabase.csv")%>%
+  as.data.table()
 
-data <- data%>%
-  as_tibble()
+#data <- arrow::read_csv_arrow("~/Documents/2021_4A_ENS_CESURE/ECB/qgfs_revisions/RealTimeDatabase.csv")%>%
+#as.data.table()
 
 VintageList <- c(outer(c("W", "G", "S", "A"), str_pad(7:20, 2, pad = "0"), FUN=paste0))
-
-GRateDB <- tibble(Index = as.Date(NA),
-                  Country = character(),
-                  Variable = character(),
-                  ECBVintage = character(),
-                  Value = numeric()
-                  )
+GRateDB <- data.table()
+vintage <- "W07"
 
 for (vintage in VintageList) {
   cat(vintage, "\n")
-  ts_cropped <- data %>% filter(ECBVintage %in% vintage)%>%
-    select(Date, VariableCode, CountryCode, Value)%>%
-    pivot_wider(names_from =  c(CountryCode, VariableCode), values_from = Value)%>%
-    arrange(Date)
   
-  ts_cropped <- xts(as.data.frame(ts_cropped[,-1]), order.by=as.Date(ts_cropped[,1]%>%pull()))
+  xts_data <- data[ECBVintage %in% vintage, c("Date", "VariableCode", "CountryCode", "Value")]%>%
+    dcast(Date ~ paste0(CountryCode, "_", VariableCode), value.var = "Value")%>%
+    as.xts.data.table()
   
-  ts_cropped_grate <- (log(ts_cropped / stats::lag(ts_cropped, 4)) * 100)["2000-01-01/"]%>% 
-    fortify.zoo() %>%
-    as_tibble() %>%
-    pivot_longer(
-      cols = !Index,
-      names_to = c("Country", "Variable"),
-      names_pattern = "(.*)_(.*)",
-      values_to = "Value"
-    )%>%
-    mutate(ECBVintage = vintage)
+  xts_growth_rate <- (log(xts_data / stats::lag(xts_data, 4)) * 100)["1999-01-01/"]%>% 
+    as.data.table() %>%
+    melt(measure.vars=patterns("(.)_(.)"), value.name = "Value")
+  xts_growth_rate[, c("Country", "Variable") := tstrsplit(variable, "_", fixed=TRUE)
+  ][, "ECB_vintage" := vintage
+  ][,variable := NULL
+  ]
+  GRateDB <- rbindlist(list(GRateDB, xts_growth_rate))
   
-  GRateDB <- bind_rows(GRateDB, ts_cropped_grate)
 
 }
 
-GRateDB <- GRateDB%>%
-  rename(Date = Index)
+setnames(GRateDB, "index", "Date")
 arrow::write_csv_arrow(x = GRateDB, "data/GRateDB.csv")
