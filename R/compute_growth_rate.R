27f4ff2fe9be42c4d8535775f56758b0dcbc0560
Thomas Faria <thomas.faria@ensae.fr>
2022-11-13 19:33:10 +0100

    add final values

--- R/compute_growth_rate.R
+++ R/compute_growth_rate.R
@@ -5,25 +5,30 @@ library(tidyr)
 library(stringr)
 library(data.table)
 source("R/functions.R")
+#data <- arrow::read_csv_arrow("https://minio.lab.sspcloud.fr/tfaria/public/RealTimeDatabase.csv")
+data <- arrow::read_csv_arrow("~/Documents/2021_4A_ENS_CESURE/ECB/qgfs_revisions/data/RealTimeDatabase.csv")
+setDT(data)
 
-data <- arrow::read_csv_arrow("https://minio.lab.sspcloud.fr/tfaria/public/RealTimeDatabase.csv")%>%
-  as.data.table()
-
-VintageList <- c(outer(c("W", "G", "S", "A"), str_pad(7:20, 2, pad = "0"), FUN = paste0))
+VintageList <- unique(data[,ECB_vintage])
+Final_vintages <- VintageList[startsWith(VintageList, "A")]
 GRateDB <- data.table()
-vintage <- "W07"
+
+reference_years <- sapply(Final_vintages, get_reference_year, simplify = FALSE, USE.NAMES = TRUE)
+for (final_vintage in c(Final_vintages)) {
+  data[(ECB_vintage %in% final_vintage) & (Date %between% reference_years[[final_vintage]]), IsFinalValue := T]
+} 
 
 for (vintage in VintageList) {
   cat(vintage, "\n")
 
-  xts_data <- data[ECBVintage %in% vintage, c("Date", "VariableCode", "CountryCode", "Value")] %>%
+  xts_data <- data[ECB_vintage %in% vintage, c("Date", "Variable_code", "Country_code", "Value")] %>%
     dcast(Date ~ paste0(CountryCode, "_", VariableCode), value.var = "Value") %>%
     as.xts.data.table()
 
   xts_growth_rate <- (log(xts_data / stats::lag(xts_data, 4)) * 100)["1999-01-01/"] %>%
     as.data.table() %>%
     melt(measure.vars = patterns("(.)_(.)"), value.name = "Value")
-  xts_growth_rate[, c("Country", "Variable") := tstrsplit(variable, "_", fixed = TRUE)
+  xts_growth_rate[, c("Country_code", "Variable_code") := tstrsplit(variable, "_", fixed = TRUE)
                   ][, "ECB_vintage" := vintage
                   ][, variable := NULL
                   ]
@@ -31,4 +36,11 @@ for (vintage in VintageList) {
 }
 
 setnames(GRateDB, "index", "Date")
+for (final_vintage in c(Final_vintages)) {
+  GRateDB[(ECB_vintage %in% final_vintage) & (Date %between% reference_years[[final_vintage]]), IsFinalValue := T]
+} 
+
+FinalValues <- rbindlist(list(GRateDB[(IsFinalValue)][, Measure := "GRate"],
+                              data[(IsFinalValue)][, Measure := "Raw"][, c("Date", "Variable_code", "Country_code", "Value", "Measure")]))
+
 arrow::write_parquet(x = GRateDB, "data/GRateDB.parquet")
