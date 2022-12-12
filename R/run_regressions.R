9c0974bc82a538e2aa831740741fd2113e1b284e
Thomas Faria <thomas.faria@ensae.fr>
2022-12-12 20:39:33 +0400

    moving functions

--- R/run_regressions.R
+++ R/run_regressions.R
@@ -20,61 +20,6 @@ Variables <- c(Var_Revenue, Var_Macro, Var_Expenditure)
 unique(RegressionDB$Variable_code)
 unique(RegressionDB$Country_code)
 
-simulate_models <- function(data, variable, list_models, include_naive, all_models) {
-  sample <- data[(Variable_code == variable)]
-
-  if (!all_models) {
-    list_models <- list_models[variable]
-  }
-
-  tryCatch(
-    expr = {
-      # We estimate a model without constant
-      model_summary <- rbindlist(lapply(list_models, function(model) {
-        estimated_model <- lm(paste(model, "-1"), data = sample)
-        summary <- broom::glance(estimated_model)
-        summary$Model_specification <- model
-        summary$N <- nobs(estimated_model)
-        summary$Variable <- variable
-        summary$RMSE <- sqrt(c(crossprod(estimated_model$residuals)) / length(estimated_model$residuals))
-        summary$Group <- unique(sample$Group)
-        return(summary)
-      }), use.names = TRUE)
-
-      if (include_naive) {
-        estimated_model <- lm("Final_revision ~ 0", data = sample)
-        naive_summary <- data.table(
-          AIC = AIC(estimated_model),
-          BIC = BIC(estimated_model),
-          N = nobs(estimated_model),
-          Model_specification = "Final_revision ~ 0",
-          Variable = variable,
-          p.value = NA,
-          RMSE = c(sqrt(crossprod(estimated_model$residuals) / length(estimated_model$residuals))),
-          Group = unique(sample$Group)
-        )
-        model_summary <- rbindlist(list(model_summary, naive_summary), use.names = TRUE, fill = TRUE)
-      }
-
-      return(model_summary)
-    },
-    error = function(e) {
-      # When there is no regressor we have to add the constant
-      estimated_model <- lm(list_models, data = sample)
-      model_summary <- data.table(
-        AIC = AIC(estimated_model),
-        BIC = BIC(estimated_model),
-        N = nobs(estimated_model),
-        Model_specification = list_models,
-        Variable = variable,
-        p.value = NA,
-        RMSE = c(sqrt(crossprod(estimated_model$residuals) / length(estimated_model$residuals))),
-        Group = unique(sample$Group)
-      )
-      return(model_summary)
-    }
-  )
-}
 
 # Faire des fonctions pour simplifier
 # finir le renommage des variables
@@ -102,58 +47,6 @@ top_models <- rbindlist(lapply(c("AIC", "BIC"), function(criterion) {
 
 top_models[Criterion == criterion, Model_specification]
 
-get_intermediate_models <- function(old_models, interm_model_number) {
-  model_str <- stringr::str_replace_all(
-    old_models, c("\\+Rev_lag1" = "", "\\+Rev_lag2" = "", "\\+Rev_lag3" = "", "\\+Rev_lag4" = "", "\\+Rev_lag5" = "")
-  ) |>
-    stringr::str_replace_all(
-      c("Rev_lag1" = "0", "Rev_lag2" = "0", "Rev_lag3" = "0", "Rev_lag4" = "0", "Rev_lag5" = "0")
-    )
-
-  if (interm_model_number == 1) {
-    return(model_str)
-  }
-
-  model_str <- stringr::str_replace_all(
-    model_str, c("\\+First_announcement" = "")
-  ) |>
-    stringr::str_replace_all(
-      c("First_announcement" = "0")
-    )
-
-  if (interm_model_number == 2) {
-    return(model_str)
-  }
-
-  model_str <- stringr::str_replace_all(
-    model_str, c("\\+ESA2010" = "")
-  ) |>
-    stringr::str_replace_all(
-      c("ESA2010" = "0")
-    )
-  if (interm_model_number == 3) {
-    return(model_str)
-  }
-
-  model_str <- stringr::str_replace_all(
-    model_str, c("\\+ObsQ_1" = "", "\\+ObsQ_2" = "", "\\+ObsQ_3" = "", "\\+ObsQ_4" = "")
-  ) |>
-    stringr::str_replace_all(
-      c("ObsQ_1" = "0")
-    )
-
-  if (interm_model_number == 4) {
-    return(model_str)
-  }
-
-  model_str <- stringr::str_replace_all(
-    model_str, c("\\+Country_DE" = "", "\\+Country_ES" = "", "\\+Country_FR" = "", "\\+Country_IT" = "", "\\+Country_NL" = "", "\\+Country_BE" = "", "\\+Country_AT" = "", "\\+Country_FI" = "", "\\+Country_PT" = "", "\\+Country_REA" = "")
-  ) |>
-    stringr::str_replace_all(
-      c("Country_DE" = "0")
-    )
-  return(model_str)
-}
 
 for (interm_model in 1:5) {
   new_models <- get_intermediate_models(top_models[, Model_specification], interm_model)
